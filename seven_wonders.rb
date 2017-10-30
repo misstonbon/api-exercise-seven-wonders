b9eec8197adb9f6c38455e77cdf1d5a220582eef
Tanja Stroble <plusdeltah@gmail.com>
2017-10-30 11:51:53 -0700

    completed

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,15 +1,21 @@
 require 'httparty'
+require 'ap'
 
-#Starter Code:
 seven_wonders = ["Great Pyramind of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
 
+BASE_URL = "https://maps.googleapis.com/maps/"
+API_KEY = "AIzaSyCEwA3FVAbzCIKRXzDcs3I34U1GIXgyV2c"
 
+def get_coordinates(place)
+  url = BASE_URL + "api/geocode/json?&address=" + "#{place}"
+  ap url
+  response = HTTParty.get(url).parsed_response
+  return response["results"].first["geometry"]["location"]
+end
 
+coordinates = {}
+seven_wonders.each do |place|
+  coordinates[place] = get_coordinates(place)
+end
 
-
-
-
-
-
-#Example Output:
-#{"Great Pyramind of Giza"=>{"lat"=>29.9792345, "lng"=>31.1342019}, "Hanging Gardens of Babylon"=>{"lat"=>32.5422374, "lng"=>44.42103609999999}, "Colossus of Rhodes"=>{"lat"=>36.45106560000001, "lng"=>28.2258333}, "Pharos of Alexandria"=>{"lat"=>38.7904054, "lng"=>-77.040581}, "Statue of Zeus at Olympia"=>{"lat"=>37.6379375, "lng"=>21.6302601}, "Temple of Artemis"=>{"lat"=>37.9498715, "lng"=>27.3633807}, "Mausoleum at Halicarnassus"=>{"lat"=>37.038132, "lng"=>27.4243849}}
+ap coordinates
